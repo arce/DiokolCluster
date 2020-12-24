bcdd7b5a7561640ce8fc87bceb807df0b23940a6
Arce <armando.arce@gmail.com>
2020-12-24 03:39:06 +0000

    Update

--- dependencies/ShivaVG-0.2.1/shDefs.h
+++ dependencies/ShivaVG-0.2.1/shDefs.h
@@ -62,14 +62,7 @@
 #    include <inttypes.h>
 #  endif
 #else
-#  define int8_t    char
-#  define uint8_t   unsigned char
-#  define int16_t   short
-#  define uint16_t  unsigned short
-#  define int32_t   int
-#  define uint32_t  unsigned int
-#  define int64_t   long long
-#  define uint64_t  unsigned long long
+#    include <stdint.h>
 #endif
 
 typedef int8_t SHint8;
